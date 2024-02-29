ee38efdf1781a4685951a4f29b583e5914bce119
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-03-01 01:42:26 +0530

    Created JwtUtil Class

---
+++ src/main/java/com/jwt/backend/util/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.jwt.backend.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "jwt_authentication";
+
+    public String getUserNameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+}
