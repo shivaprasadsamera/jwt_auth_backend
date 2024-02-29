1ca6af24012253ad698f318036c08d69178c9b13
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-03-01 03:27:22 +0530

    Updated UserController Class

--- src/main/java/com/jwt/backend/controller/UserController.java
+++ src/main/java/com/jwt/backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.jwt.backend.controller;
 import com.jwt.backend.entity.User;
 import com.jwt.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,11 +28,13 @@ public class UserController {
     }
 
     @GetMapping({"/forAdmin"})
+    @PreAuthorize("hasRole('Admin')")
     public String forAdmin(){
         return "This URL is accessible for admin only!";
     }
 
     @GetMapping({"/forUser"})
+    @PreAuthorize("hasRole('User')")
     public String forUser(){
         return "This URL is accessible for user only!";
     }
