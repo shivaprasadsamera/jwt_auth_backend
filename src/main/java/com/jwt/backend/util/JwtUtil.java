a83140b0d72a79f09722f39d10d129b40c42ccfa
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-03-01 02:44:20 +0530

    Updated JwtUtil Class

--- src/main/java/com/jwt/backend/util/JwtUtil.java
+++ src/main/java/com/jwt/backend/util/JwtUtil.java
@@ -2,16 +2,20 @@ package com.jwt.backend.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Component
 public class JwtUtil {
 
     private static final String SECRET_KEY = "jwt_authentication";
+    private static final int TOKEN_VALIDITY = 3600 * 5;
 
     public String getUserNameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
@@ -38,6 +42,18 @@ public class JwtUtil {
 
     private Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
     }
 
 }
