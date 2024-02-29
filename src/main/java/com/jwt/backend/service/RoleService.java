7df51ba8b76ad640eb2f81257a3b77393e6d467c
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-02-29 13:56:54 +0530

    Created and added RoleService class

---
+++ src/main/java/com/jwt/backend/service/RoleService.java
@@ -0,0 +1,17 @@
+package com.jwt.backend.service;
+
+import com.jwt.backend.entity.Role;
+import com.jwt.backend.dao.RoleDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleService {
+
+    @Autowired
+    private RoleDao roleDao;
+
+    public Role createNewRole(Role role) {
+        return roleDao.save(role);
+    }
+}
