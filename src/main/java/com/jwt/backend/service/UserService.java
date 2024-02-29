34d64a22fbfd449d3862b2f14881a5e905cc8600
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-03-01 00:41:40 +0530

    Created and added User Service class

---
+++ src/main/java/com/jwt/backend/service/UserService.java
@@ -0,0 +1,57 @@
+package com.jwt.backend.service;
+
+import com.jwt.backend.dao.RoleDao;
+import com.jwt.backend.dao.UserDao;
+import com.jwt.backend.entity.Role;
+import com.jwt.backend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private RoleDao roleDao;
+    public User registerNewUser(User user){
+        return userDao.save(user);
+    }
+
+    public void initRolesAndUser(){
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("This is default admin role!");
+        roleDao.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("This is default user role!");
+        roleDao.save(userRole);
+
+        User adminUser = new User();
+        adminUser.setUserName("admin");
+        adminUser.setUserPassword("admin");
+        adminUser.setUserFirstName("admin");
+        adminUser.setUserLastName("admin");
+        Set<Role> adminRoles = new HashSet<>();
+        adminRoles.add(adminRole);
+        adminUser.setRole(adminRoles);
+        userDao.save(adminUser);
+
+        User user = new User();
+        user.setUserName("user");
+        user.setUserPassword("user");
+        user.setUserFirstName("user");
+        user.setUserLastName("user");
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+        user.setRole(userRoles);
+        userDao.save(user);
+    }
+
+}
