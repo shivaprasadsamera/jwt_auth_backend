9d0966e18515b95a2dd4554821502f28763ecdcb
shivaprasadsamera <shivaprasadsamera45@gmail.com>
2024-03-01 00:41:29 +0530

    Created and added User Controller class

---
+++ src/main/java/com/jwt/backend/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.jwt.backend.controller;
+
+import com.jwt.backend.entity.User;
+import com.jwt.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void initRolesAndUsers(){
+        userService.initRolesAndUser();
+    }
+
+    @PostMapping({"/registerNewUser"})
+    public User registerNewUser(@RequestBody User user){
+        return userService.registerNewUser(user);
+    }
+
+    @GetMapping({"/forAdmin"})
+    public String forAdmin(){
+        return "This URL is accessible for admin only!";
+    }
+
+    @GetMapping({"/forUser"})
+    public String forUser(){
+        return "This URL is accessible for user only!";
+    }
+
+}
